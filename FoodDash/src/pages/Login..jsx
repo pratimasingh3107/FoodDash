80834d34ce386bd6a972932c3fd64a5027092934
Pratima Singh <pratimas.3107@gmail.com>
2026-01-02 18:23:30 +0000

    login.jsx added

---
+++ FoodDash/src/pages/Login..jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+
+function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert(`Logged in as ${email}`);
+  };
+
+  return (
+    <div className="login-page">
+      <h2>Login</h2>
+
+      <form className="login-form" onSubmit={handleSubmit}>
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
